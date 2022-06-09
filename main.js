9ac43c572ef1631f89d4cb1f3afa4547fed750c2
RanGedamo <mcrangedamo@gmail.com>
2022-06-09 09:07:58 +0300

    first

---
+++ main.js
@@ -0,0 +1,32 @@
+// var x = 0;
+// var stop1;
+// playanimation.onclick = function moveBox() {
+//     stop1 = setInterval(() => {
+//         box.style.height = x++ + 'px';
+//     }, 10)
+// }
+
+// stopanimation.onclick = function stopbox(){
+//     clearInterval(stop1)
+// }
+
+// document.body.onkeydown = function keystop(event){
+
+//     if(event.key == 'Enter'){
+//         clearInterval(stop1)
+//     }
+// }
+
+box2.onclick = function  setBg() {
+    var arraycolors1 = ['red','blue','green','yellow']
+    var randomColor = Math. floor(Math. random()*5);
+    box2. style. backgroundColor = arraycolors[randomColor1]
+}
+
+
+
+
+
+
+// box.style.left = "508px"
+// box.style.top = "458px"
